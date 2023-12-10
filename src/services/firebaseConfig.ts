2435ebacbee6395ee238ed9f74456a36dbe8da6f
Marcos Vitor Coelho Pereira <84048852+marcosVitorCoelho@users.noreply.github.com>
2023-12-10 14:08:47 -0300

    Update firebaseConfig.ts

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUm-70YmUk6G39C8NtlO2pg92eyAjOQgY",
-  authDomain: "teste-taugor-b04ed.firebaseapp.com",
-  projectId: "teste-taugor-b04ed",
-  storageBucket: "teste-taugor-b04ed.appspot.com",
-  messagingSenderId: "242463795310",
-  appId: "1:242463795310:web:453dda85fb0cac1e709e7f",
-  measurementId: "G-5NP6Q1VVLC"
+
 };
 
 const app = initializeApp(firebaseConfig);
